7e5e45dfb1b3483d329a164b55f649a1aff35004
kizya007 <kizya007@mail.ru>
2023-09-13 14:56:24 +0200

    create todo-list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="header">
+      <img class="header-img" src="./assets/img/todoImg.png" alt="" />
+      <div class="header-date">
+        <p class="header-date__week">Thur 9</p>
+        <p class="header-date__time">6:23</p>
+      </div>
+    </div>
+    <div class="todo-content">
+      <input class="todo-content__input" type="text" placeholder="Note" />
+      <div class="todo-content__icon">
+        <plusIcon />
+        <arrowBottom />
+      </div>
+    </div>
+    <ul class="todo-list">
+      <TodoMain />
+      <TodoMain />
+      <TodoMain />
+      <TodoMain />
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoMain from "./components/TodoMain.vue";
+import plusIcon from "./assets/icon/plusIcon.vue";
+import arrowBottom from "./assets/icon/arrowBottom.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodoMain,
+    plusIcon,
+    arrowBottom,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./assets/App.scss";
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;500;600;700&family=Russo+One&display=swap");
 </style>
---
+++ src/components/TodoMain.vue
@@ -0,0 +1,29 @@
+<template>
+  <li class="todo">
+    <div class="todo-text">
+      <p class="todo-text__title">Dinner</p>
+      <p class="todo-text__time">Today at 8:00 PM</p>
+    </div>
+    <div class="todo-icons">
+      <circleIcon />
+      <garbageIcon />
+    </div>
+  </li>
+</template>
+
+<script>
+import circleIcon from "../assets/icon/circleIcon.vue";
+import garbageIcon from "../assets/icon/garbageIcon.vue";
+
+export default {
+  name: "TodoMain",
+  components: {
+    circleIcon,
+    garbageIcon,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../assets/todo.scss";
+</style>
